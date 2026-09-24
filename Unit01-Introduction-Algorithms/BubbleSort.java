fd8cbda84a123e19f9eca86173900d2d929541b8
AKSHAT THAKUR <165548880+AKSHATTHAKUR2145@users.noreply.github.com>
2026-09-25 00:55:21 +0530

    Add files via upload

---
+++ Unit01-Introduction-Algorithms/BubbleSort.java
@@ -0,0 +1,21 @@
+package Unit01_Introduction_Algorithms.Sorting;
+
+public class BubbleSort {
+
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
